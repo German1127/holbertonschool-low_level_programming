4d3d96dae84ddd38329f56eff75368e9259b4a25
German1127 <kleverk2023@gmail.com>
2023-06-14 10:30:46 -0700

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,7 +2,7 @@ gcc
 #incude<stdio.h>
 int main() 
 {
-	 putchar (Programming is like building a multilingual puzzle);
+	 puts (Programming is like building a multilingual puzzle);
 	      
 	 return 0;
 }
