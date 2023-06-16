93bcea85db13827bc1f6a15d9aeee52b2669eb30
German1127 <kleverk2023@gmail.com>
2023-06-16 15:47:30 -0700

    3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter<= 'z'; letter++)
+{
+printf("%c", letter);
+}
+printf("\n");
+for (letter = 'A'; letter <= 'Z'; letter++)
+{
+printf("%c", letter);
+}
+printf("\n");
+return(0);
+}
