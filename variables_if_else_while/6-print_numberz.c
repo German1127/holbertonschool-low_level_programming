85c1566c86e53ebf0970ce99db23d9142e1d598e
German1127 <kleverk2023@gmail.com>
2023-06-17 08:11:59 -0700

    6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -6,18 +6,12 @@
 */
 int main() 
 {
-int numero;
-char numeros[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-char espacio = ' ';
+	int number;
 
-write(1, "Números de un dígito en base 10: ", 32);
-
-for (numero = 0; numero < 10; numero++) {
-write(1, &(numeros[numero]), 1);
-write(1, &espacio, 1);
-}
-
-write(1, "\n", 1);
-
-return 0;
+	for (number = 0; numero < 10; number ++)
+	{
+		putchar(number + '0');
+	}
+	putchar('\n');
+	return(0);
 }
