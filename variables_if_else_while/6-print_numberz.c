80fd7ee413ee3183299dd5ce35c41f6020a97de7
German1127 <kleverk2023@gmail.com>
2023-06-17 08:05:24 -0700

    6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -4,19 +4,20 @@
 *
 * Return: always 0 (success)
 */
-int main ()
+int main() 
 {
-	int number;
-	char buffer[2];
+int numero;
+char numeros[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+char espacio = ' ';
 
-	write(1,"Base 10 single digit numbers	10:\n ",32);
+write(1, "Números de un dígito en base 10: ", 32);
 
-	for (number = 0; number < 10; number++)
-	{
-		buffer[0] = '0' + number;
-		buffer[1] = ' ';
-		write(1, buffer, 2);
-	}
-	write(1,"\n", 1);
-	return(0);
+for (numero = 0; numero < 10; numero++) {
+write(1, &(numeros[numero]), 1);
+write(1, &espacio, 1);
+}
+
+write(1, "\n", 1);
+
+return 0;
 }
