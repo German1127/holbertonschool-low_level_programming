b44872e3087f253123c61e48860b3c414bb5eda8
German1127 <kleverk2023@gmail.com>
2023-06-17 06:40:48 -0700

    5

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -4,12 +4,17 @@
  *
  * Return: always 0 (success)
  */
-int main(void){
+int main(void)
+{
+
 int i;
+
 printf("base digits 10:");
-for(i = 0; i < 10; i++){
-printf("%d ", i);
+for(i = 0; i < 10; i++)
+{
+printf("%d", i);
 }
 printf("\n");
+
 return (0);
 }
