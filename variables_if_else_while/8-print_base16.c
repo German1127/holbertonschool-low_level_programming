3a1272102b77a703335af2d5445a1af58b1d0db1
German1127 <kleverk2023@gmail.com>
2023-06-17 10:02:31 -0700

    8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		putchar(digit + '0');
+	}
+	for (digit = 0; digit < 6; diggit ++)
+	{
+		putchar(digit + 'a');
+	}
+	putchar('\n');
+	return (0);
+}
