07f6f826150d4babef699177f323c6f18c175d0d
German1127 <kleverk2023@gmail.com>
2023-06-17 06:29:57 -0700

    5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+int i;
+
+printf("base digits 10: ");
+if (i = 0; i < 10; i++)
+{
+printf("%d ", i);
+}
+printf("\n");
+return (0);
+}
