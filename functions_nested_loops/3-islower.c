59f6cfd5ba66a5fd7cadadadf40c7728a8de10fb
German1127 <kleverk2023@gmail.com>
2023-06-18 13:36:17 -0700

    3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - 
+ *
+ * Return: lowercase
+ */
+int _islower(int c)
+{
+	char x;
+	int lowercase = 0;
+
+	for (x = 'a'; x <= 'z'; ++)
+		if (x == c)
+			lowercase =1;
+	return (lowercase);
+}
