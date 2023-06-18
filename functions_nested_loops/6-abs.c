5b9e22d67ca11e2e321edffe39125bc07fdaa80a
German1127 <kleverk2023@gmail.com>
2023-06-18 14:03:31 -0700

    6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - Checker
+ *
+ * @r: Parameter to check
+ *
+ * Return: Absolute of r
+ */
+int _abs(int)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (r + -1);
+}
