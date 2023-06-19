890d022d1010164c92600d8d1a5c9cafdc04c82c
German1127 <kleverk2023@gmail.com>
2023-06-19 12:15:40 -0700

    9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,18 +7,18 @@ void times_table(void)
 {
 	int a, b, c;
 
-	for(a = 0; a <= 9; a++)
+	for (a = 0; a <= 9; a++)
 	{
-		for(b = 0; b <= 9; b++)
+		for (b = 0; b <= 9; b++)
 		{
 			c = a * b;
 			if (c < 10)
 			{
-				if( b != 0)
+				if ( b != 0)
 					_putchar(' ');
 				_putchar(c + 48);
 
-				if (b ==9)
+				if (b == 9)
 					continue;
 				_putchar(',');
 				_putchar(' ');
