222e59bd74336858da329f2445bb94c93cf34433
German1127 <kleverk2023@gmail.com>
2023-06-19 11:50:16 -0700

    9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+	int i;
+	for (i = 0; i <= 10; i++)
+	{
+		printf("9 x %d = %d\n", i 9 * i);
+	}
+	return (0);
+}
