edeb93392b75bd99dad1e761c351e2c3b7dc1082
German1127 <kleverk2023@gmail.com>
2023-06-19 14:29:14 -0700

    11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_to_98(int n)
+{
+	int n;
+
+	for(n = 0; n <= 98; n++)
+	{
+		printf("%d\n", n);
+	}
+	return (0);
+}
