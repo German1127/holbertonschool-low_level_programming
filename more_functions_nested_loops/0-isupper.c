ad0f0956c8c38edb4ce95c5765ff319831ab893c
German1127 <kleverk2023@gmail.com>
2023-06-19 19:10:49 -0700

    push

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctipo.h>
+/**
+ *
+ *
+ */
+int _isupper(int c)
+{
+	{
+if (isupper(c))
+printf("0");
+	}
+{
+	else(isupper(C))
+		printf("1");
+}
+}
