9d07bae536030acf46edded55b971986f289b66d
German1127 <kleverk2023@gmail.com>
2023-06-20 02:25:22 -0700

    6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- *  * print_line - Checker
- *   *
- *    * @n: Parameter to check
- *     *
- *      * Return: Always 0
- *       */
+ * print_line - Checker
+ *
+ * @n: Parameter to check
+ *
+ * Return: Always 0
+ */
 void print_line(int n)
 {
 	int x = 0;
 
 	if (n > 0)
 	{
-		for(; x < n; x++)
+		for (; x < n; x++)
 			_putchar('_');
 	}
 	_putchar('\n');
