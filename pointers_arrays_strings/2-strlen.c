4921630a771b9ec52822ed8381139068c7c05687
German1127 <kleverk2023@gmail.com>
2023-06-22 16:21:12 -0700

    2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,17 @@
+#include <unistd.h>
 #include "main.h"
-#include <string.h>
 /**
- * _strlen - Program entry point
+ * _strlen - longitud
  * @s: int
- * Return: Returns 0 if the execution was successful
+ *
+ * Return: void
+ *
  */
 int _strlen(char *s)
 {
-int i;
-int cont = 0;
-for (i = 0; s[i] != '\0'; i++);
-cont++;
-return (cont);
+	int i;
+	int count = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	count++;
+return (count);
 }
