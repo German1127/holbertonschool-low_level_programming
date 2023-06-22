4db5b20ae2e4fb825eb3a5ccc596b07b3e415c13
German1127 <kleverk2023@gmail.com>
2023-06-22 07:29:35 -0700

    2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * main - Program entry point
+ *
+ * Return: Returns 0 if the execution was successful
+ */
+int _strlen(char *s)
+{
+const char *s = "My first strlen!";
+size_t length = strlen *s;
+_putchar("Long de la cadena: %zu\n", length);
+return (0);
+}
+	
