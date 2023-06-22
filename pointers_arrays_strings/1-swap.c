be4465bc038f1d3c155b3b4caba3f2106d72c6d4
German1127 <kleverk2023@gmail.com>
2023-06-22 06:44:41 -0700

    1

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -4,14 +4,8 @@
  *
  * Return: Always 0.
  */
-void swap_int(int *a, int *b)
-{
-
-int a = 98;
-int b = 42;
-
-_putchar("a=%d, b=%d\n", a, b);
-swap_int(&a, &b);
-_putchar("a=%d, b=%d\n", a, b);
-return (0);
+void swap_int(int *a, int *b){
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
