9b8c1421877957427eda3457e1b39fe27e762b6d
German1127 <kleverk2023@gmail.com>
2023-06-23 09:13:33 -0700

    7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -6,26 +6,26 @@
  */
 void puts_half(char *str)
 {
-int a = 0;
-int b = 0;
+	int a = 0;
+	int b = 0;
 
-while (str[a] != '\0')
-	a++;
+	while (str[a] != '\0')
+		a++;
 
-a -= 1;
+	a -= 1;
 
-if (a % 2 == 0)
-{
-	b = (a + 1) / 2;
-}
-else
-{
-	b = (a + 1) / 2;
-}
-while (str[b] != '\0')
-{
-_putchar(str[b]);
-	b++;
-}
-_putchar('\n');
+	if (a % 2 == 0)
+	{
+		b = a / 2;
+	}
+	else
+	{
+		b = (a + 1) / 2;
+	}
+	while (str[b] != '\0')
+	{
+		_putchar(str[b]);
+		b++;
+	}
+	_putchar('\n');
 }
