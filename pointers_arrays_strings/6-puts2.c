3ed73f3030e6b3a780432c356ffd508038d05b4e
German1127 <kleverk2023@gmail.com>
2023-06-23 04:00:13 -0700

    6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+	int a;
+	int b = 0;
+	for (a = 0; str[a] != '\0'; a++)
+
+	a -= 1;
+
+	for (b = 0; b <= a; b += 2)
+		_putchar(str[a]);
+
+	_putchar ('\n')
+}
