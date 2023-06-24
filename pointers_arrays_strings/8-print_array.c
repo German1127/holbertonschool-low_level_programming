8baaa26834d620d152e7d5d1c446d85b7d29efd3
German1127 <kleverk2023@gmail.com>
2023-06-24 08:35:53 -0700

    8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- *
+ * print_array - XD
  *
  */
 void print_array(int *a, int n)
@@ -9,7 +9,7 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", a[i]);
+		printf("%d, ", a[i]);
 }
 printf("\n");
 }
