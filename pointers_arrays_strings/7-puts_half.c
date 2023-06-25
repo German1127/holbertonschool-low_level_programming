60c21c29c946d57803dc5bc58fb1eec82fc4ef18
German1127 <kleverk2023@gmail.com>
2023-06-25 13:16:44 -0700

    7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,30 +1,23 @@
 #include "main.h"
 /**
- * puts_half - Prints the second half of a string.
- * Description: This function takes a pointer to a character
- *@str: Pointer to the character array.
+ * puts_half - Prints the second half.
+ *
+ * @str: Pointer to the character array.
+ *
  */
 void puts_half(char *str)
 {
-	int a = 0;
-	int b = 0;
+	int len = 0, x = 0, y = 0;
 
-	while (str[a] != '\0')
-		a++;
+	while (str[len] != '\0')
+		len++;
 
-	a -= 1;
-
-	if (a % 2 == 0)
-	{
-		b = a / 2;
-	}
+	if (len % 2 == 0)
+		x = len / 2;
 	else
-	{
-		b = (a + 1) / 2;
-	}
-	for (; b <= a; b++)
-	{
-		_putchar(str[b]);
-	}
+		x = ((len + 1) / 2);
+
+	for (y = x; y < len; y++)
+		_putchar(str[y]);
 	_putchar('\n');
 }
