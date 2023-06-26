115b7654cf5ea3fe434c58240742bba89477c9fd
German1127 <kleverk2023@gmail.com>
2023-06-26 10:43:07 -0700

    3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * _strcmp - Compare two strings
+ * @s1: First string
+ * @s2: Second string
+ *
+ * Return: Difference between the first pair of different characters
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+
+				*s1++ ; *s2++
+	}
+	return (*s1 - *s2);
+}
