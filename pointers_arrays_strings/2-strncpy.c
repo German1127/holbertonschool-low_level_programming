a36a4908d8f08a9f14a8eeb8d0470384c54b1c48
German1127 <kleverk2023@gmail.com>
2023-06-26 07:21:46 -0700

    2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncpy - Copia la cadena src en la cadena dest, limitada por n bytes.
+ * @dest: Cadena de destino.
+ * @src: Cadena de origen.
+ * @n: Número máximo de bytes a copiar.
+ *
+ * Return: Puntero a la cadena de destino (dest).
+ */
+char *_strncpy(char *dest, char *src, int n)i
+{
+	    int i;
+
+		for (i = 0; i < n && src[i] != '\0'; i++)
+		{
+			dest[i] = src[i];
+
+		}
+
+		while (i < n)
+		{
+			dest[i] = '\0';
+			i++;
+
+		}
+
+		return (dest);
+}
