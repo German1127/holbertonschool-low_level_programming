000950a8f9576a3edc2c4fd0bf955cb502851701
German1127 <kleverk2023@gmail.com>
2023-06-27 05:35:34 -0700

    6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,25 +1,26 @@
 #include "main.h"
 /**
  * cap_string - Write a function that capitalizes all words of a string.
- *
- * return - return (s)
+ * @s - puntero
+ * Return - return (s)
  *
  */
 char *cap_string(char *s)
 {
-	char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}','"'};
+	char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
 	int chek = 0;
 	int chek2 = 13;
 	int cont;
 
 	while (s[chek])
 {
-cont = 0
+cont = 0;
+
 	while (cont < chek2)
 	{
-		if ((chek == 0 || s[chek - 1] == spc[cont]) && (s[chek] >= 'a' && s[chek] =< 'z'))
+	if ((chek == 0 || s[chek - 1] == spc[cont]) && (s[chek] >= 'a' && s[chek] <= 'z'))
 			s[chek] -= 32;
-	
+
 
 cont++;
 }
