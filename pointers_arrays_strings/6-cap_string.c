697b700dd23f623564224114bc0e708217ef59a4
German1127 <kleverk2023@gmail.com>
2023-06-27 05:09:09 -0700

    6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ */
+char *cap_string(char *s)
+{
+	char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};'"'};
+	int chek = 0;
+	int chek2 = 13;
+	int cont;
+
+	while (s[chek])
+{
+cont = 0
+	while (cont < chek2)
+	{
+		if ((chek == 0 || s[chek - 1] == spc[cont]) && (s[chek] >= 'a' && s[chek] =< 'z'))
+			s[chek] -= 32;
+	
+	
+cont++;
+}
+chek++;
+}
+return (s);
+}
