62b0671d178fba15e99afb7126dbaada22c699e3
German1127 <kleverk2023@gmail.com>
2023-06-26 19:56:44 -0700

    5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - Convierte una cadena de caracteres a mayúsculas
+ * @s: La cadena de caracteres a convertir
+ *
+ * Return: Puntero a la cadena convertida
+ */
+char *string_toupper(char *s)
+{
+	char *x = s;
+
+	while (*s)
+	{
+		if (*s >= 'a' && *s <= 'z')
+			*s -= 32; // el -32 es la convercion del codigo ascii
+
+		s++; // para que si son minusculas pasen a mayusculas
+	}
+	return (x);
+}
