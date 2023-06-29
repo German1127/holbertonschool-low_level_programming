ef54dd7f8954598d7296c6b573fdad12ed634c83
German1127 <kleverk2023@gmail.com>
2023-06-29 08:41:39 -0700

    5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _sqrt - raiz cuadrada
+ * @n: int
+ * @m: int
+ * Return: 0
+ */
+int _sqrt(int n, int m)
+{
+	if ((n * 10000) - (m * m) <= 0)
+	{
+		return (m / 100);
+	}
+	else
+		return (_sqrt(n, m + 1));
+
+	/**
+	 * _sqrt_recursion - xd
+	 * @n: int
+	 * Return: 0
+	 */
+}
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (_sqrt(n, n / 2));
+}
