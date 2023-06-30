a694a776a0ba46cef04b5aa751cde85583b73afa
German1127 <kleverk2023@gmail.com>
2023-06-30 09:57:16 -0700

    0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *  *main - prints the name of the funtion
- *   *@argc: how many arguments are
- *    *@argv: the string
- *     *Return: 0
- *      */
+ *main - prints the name of the funtion
+ *@argc: how many arguments are
+ *@argv: the string
+ *Return: 0
+ */
 int main(int argc, char *argv[])
 {
 	int i;
@@ -13,8 +13,6 @@ int main(int argc, char *argv[])
 	{
 	printf("%s", argv[0]);
 	printf("\n");
-							
 	}
 	return (0);
-				
 }
