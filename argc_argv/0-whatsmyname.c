fbcdf0a0124d0cc7133e50c3e966f45a6c0b003b
German1127 <kleverk2023@gmail.com>
2023-06-30 09:53:30 -0700

    push

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *  *main - prints the name of the funtion
+ *   *@argc: how many arguments are
+ *    *@argv: the string
+ *     *Return: 0
+ *      */
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+	printf("%s", argv[0]);
+	printf("\n");
+							
+	}
+	return (0);
+				
+}
