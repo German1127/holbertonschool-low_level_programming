added751e55a8f1ab6ba03dd352c0fa7a9786259
German1127 <kleverk2023@gmail.com>
2023-07-10 13:04:25 -0700

    1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+/*
+ * init_dog - Initializes a dog structure
+ * @d: Pointer to the dog structure
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Description: This function initializes the fields of a dog structure
+ *              with the provided name, age, and owner.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+{
+	struct init_dog d;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
+	return (void);
+}
