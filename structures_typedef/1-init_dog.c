3d5f4310ded9f9fbfaded154f50fecfe6e7e0e48
German1127 <kleverk2023@gmail.com>
2023-07-10 13:13:55 -0700

    1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -9,9 +9,9 @@
  * Description: This function initializes the fields of a dog structure
  *              with the provided name, age, and owner.
  */
-void init_dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-if (d != NULL)
+	if (d != NULL)
 	{
 		d->name = name;
 		d->age = age;
