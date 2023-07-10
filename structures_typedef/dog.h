b2ad399d4e0a2e5164503ea0310a6141a8d0eaa9
German1127 <kleverk2023@gmail.com>
2023-07-10 11:02:46 -0700

    0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG.H
+/**
+ * struct dog - xdxdxd
+ * @name: uhuhuhu
+ * @age: huhuhuh
+ * @owner: ijijijiji
+ */
+struct dogi
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
