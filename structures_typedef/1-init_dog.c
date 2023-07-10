22df44e9ec368f66efe21a24cdde14e765d6293c
German1127 <kleverk2023@gmail.com>
2023-07-10 13:11:17 -0700

    1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -11,11 +11,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 {
-	struct init_dog d;
-
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-
-	return (void);
+if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
