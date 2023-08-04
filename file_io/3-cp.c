322bb1981502018f73d8dd5958867ea9dc259ff9
German1127 <kleverk2023@gmail.com>
2023-08-04 05:58:47 -0700

    Task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -43,9 +43,9 @@ int main(int argc, char *argv[])
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
 	}
 	if (close(file_from) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
 	if (close(file_to) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
 
 	return (0);
 }
